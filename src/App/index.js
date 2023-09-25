c7f88d4a9b4f07358b43f8fc9e061fcf3e5b4dcf
Fabricio Daniel Turrina <fabriciodanielturrina@gmail.com>
2023-09-25 13:09:12 -0300

    commit

--- src/App/index.js
+++ src/App/index.js
@@ -1,69 +1,84 @@
-import './App.css';
+import "./App.css";
 import React, { useState } from "react";
-import { AppUI } from './AppUI';
+import { AppUI } from "./AppUI";
+
+function useLocalStorage(itemName, initialValue) {
+  //traemos nuestros todos almacenados
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
+
+  if (!localStorageItem) {
+    //si local storage esta vacio, osea es usuario nuevo, seteamos un "initialValue" (es un tipo texto con un array vacio) bajo el nombre de itemName y a parsedItem lo iniciamos con un array vacio
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  } else {
+    // si local storage no esta vacio asignamos a la var 'parsedItem' lo que hay en local storage parseado a json
+    parsedItem = JSON.parse(localStorageItem);
+  }
+  
+  const [item, setItem] = useState(parsedItem);
+
+  const saveItem = (newItem) => {
+    const stringifiedItem = JSON.stringify(newItem);
+    localStorage.setItem(itemName, stringifiedItem);
+    setItem(newItem);
+  };
+
+
+  return [
+    item,
+    saveItem,
+  ]
+
+}
 
 function App() {
-//traemos nuestros todos almacenados
-const localStorageTodos = localStorage.getItem('TODOS_V1')  
-let parsedTodos;
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', [])
 
-if(!localStorageTodos){
-  //si local storage esta vacio, osea es usuario nuevo, seteamos un "[]" (es un tipo texto con un array vacio) bajo el nombre de TODOS_V1 y a parsedTodos lo iniciamos con un array vacio
-  localStorage.setItem('TODOS_V1', JSON.stringify([]))
-  parsedTodos = []
-}else{
-  // si local storage no esta vacio asignamos a la var 'parsedTodos' lo que hay en local storage parseado a json
-  parsedTodos = JSON.parse(localStorageTodos)
-}
+  const [searchValue, setSearchValue] = useState("");
 
+  const completedTodos = todos.filter((todo) => !!todo.completed).length;
+  const totalTodos = todos.length;
 
- const [searchValue , setSearchValue] = useState('')
- const [todos , setTodos] = useState(parsedTodos)
+  let searchedTodos = [];
 
- const completedTodos = todos.filter(todo => !!todo.completed).length
- const totalTodos = todos.length
-
- let searchedTodos = []
-
- if (!searchValue.length >= 1) {
-  searchedTodos = todos;
-} else {
-  searchedTodos = todos.filter(todo => {
-    const todoText = todo.text.toLowerCase();
-    const searchText = searchValue.toLowerCase();
-    return todoText.includes(searchText);
-  });
-}
-
- const completeTodo = (text) =>{
-  const todoIndex = todos.findIndex(todo => todo.texto === text)
-
-  if (todoIndex !== -1) {
-    const newTodos = [...todos]; // Clona el array antes de modificarlo
-    newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
-    setTodos(newTodos);
+  if (!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter((todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
   }
 
- }
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex((todo) => todo.texto === text);
 
- const deleteTodo = (text) =>{
-  const todoIndex = todos.findIndex(todo => todo.texto === text)
-  const newTodos = [...todos]
-  newTodos.splice(todoIndex, 1)  
-  setTodos(newTodos)
- }
+    if (todoIndex !== -1) {
+      const newTodos = [...todos]; // Clona el array antes de modificarlo
+      newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
+      saveTodos(newTodos);
+    }
+  };
+
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex((todo) => todo.texto === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
+    saveTodos(newTodos);
+  };
 
   return (
-
     <AppUI
-    totalTodos={totalTodos}
-    completedTodos={completedTodos}
-    searchValue={searchValue}
-    setSearchValue={setSearchValue}
-    searchedTodos={searchedTodos}
-    completeTodo={completeTodo}
-    deleteTodo={deleteTodo}
-  />
+      totalTodos={totalTodos}
+      completedTodos={completedTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
   );
 }
 
