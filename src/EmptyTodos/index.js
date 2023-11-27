ad197078e48566b482647bd7b750c21b4102d923
Fabricio Daniel Turrina <fabriciodanielturrina@gmail.com>
2023-11-27 12:50:09 -0300

    commit

--- src/EmptyTodos/index.js
+++ src/EmptyTodos/index.js
@@ -4,37 +4,14 @@ import { CreateTodoButton } from '../CreateTodoButton'
 
 function EmptyTodos ( ) {
 
-  
- 
   return (
     <>
-      <h2>!Crea tu primer TO-DO!</h2>
-      {/* <CreateTodoButton    />  */}
+    <div className='containerEmpty'>
+      <h2>No tienes tareas, crea una nueva!</h2>
+    </div>
     </>
    
   )
 }
 
 export {EmptyTodos} 
-// const customCSS = {
-		
-  //   backgroundColor: "rgb(156, 98, 59)",
-  //   boxShadow:"0px 0px 5px 5px rgba(226, 96, 9, 0.466)",
-  //   border: "none",
-  //   borderRadius: "50%",
-  //   cursor: "pointer",
-  //   fontSize: "60px",
-  //   position: "fixed",
-  //   bottom: "24%",
-  //   fontWeight: "bold",
-  //   color: "#fafafa",
-  //   display: "flex",
-  //   justifyContent: "center",
-  //   alignItems: "center",
-  //   height: "84px",
-  //   width: "84px",
-    
-  //   transform: "rotate(0)",
-  //   transition: "0.3s ease-in-out",
-  // }
-  
--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -5,8 +5,10 @@ import './TodoCounter.css';
 function TodoCounter( {completedTodos, totalTodos}) {
     
     return(
-
-        <h2  className="TodoCounter">Has completado {completedTodos} de {totalTodos} Tareas.</h2>
+        <div className="containerTodoCounter">
+             <h3  className="TodoCounter">Has completado {completedTodos} de {totalTodos} Tareas.</h3>
+        </div>
+       
     )
     
 }
